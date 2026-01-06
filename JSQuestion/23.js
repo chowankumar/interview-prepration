f335494185acf3e33c5ba4e175b161ac23948bac
chowan kumar <chowanmalhani5@gmail.com>
2026-01-06 18:13:23 +0500

    add questions of the html

--- JSQuestion/23.js
+++ JSQuestion/23.js
@@ -12,4 +12,4 @@ console.log(counter());
 console.log(counter());
 console.log(counter());
 
-//closure : is created when a function remembers the variables from uts outer scope even after that outer function has finshed executing.
+//closure : is created when a function remembers the variables from its outer scope even after that outer function has finshed executing.
