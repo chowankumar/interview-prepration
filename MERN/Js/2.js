0dbb88b8acf7ee4706ae537147ea4e4b5b20665e
chowan kumar <chowanmalhani5@gmail.com>
2026-01-07 18:39:02 +0500

    do the q2 of the js

---
+++ MERN/Js/2.js
@@ -0,0 +1,17 @@
+// Print the numbers from 1 to 5 to the console using a loop.
+for(let i =1;i<=5;i++){
+    console.log(i)
+}
+
+let j = 1
+while(j<=5){
+    console.log(j);
+    j++;
+}
+
+ let z = 1;
+do{
+   
+    console.log(z);
+    z++
+}while(z<=5)
