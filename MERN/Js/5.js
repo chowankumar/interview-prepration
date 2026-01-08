4c51bb699d0076c627c7cb3ab1f99aba41b86f04
chowan kumar <chowanmalhani5@gmail.com>
2026-01-08 16:19:42 +0500

    q5 of the js

---
+++ MERN/Js/5.js
@@ -0,0 +1,10 @@
+const reverseString = (str)=>{
+    const revStr = str.split("").reverse().join("");
+    console.log(revStr)
+
+}
+
+console.log(reverseString("chowan"))
+
+//split change string to array
+//join change array to string
