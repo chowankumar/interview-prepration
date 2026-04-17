ea6cd9c99e8ba96b2ef6c74a7cbc18eaf1c0add3
chowan kumar <chowanmalhani5@gmail.com>
2026-04-17 13:44:48 +0500

    add the question of the js

--- JSQuestion/50.js
+++
@@ -1,8 +0,0 @@
-//Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
-
-function fraction(numerator,denominator){
-const res = numerator/denominator;
-return res.toString();
-}
-
-console.log(typeof fraction(1,2));
